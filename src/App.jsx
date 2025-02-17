8e2cafcab92063637cf311bac1d6a2b44c4f5ccb
wingchichan <w.chi.chan@gmail.com>
2025-02-17 15:45:56 +0000

    dynamic routes user/ username

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Navigation from "./pages/Navigation";
+import UserPage from "./pages/UserPage";
 
 export default function App() {
   return (
@@ -9,6 +10,8 @@ export default function App() {
       <Routes>
         <Route path={"/"} element={<h2>Home Page</h2>} />
         <Route path={"/about"} element={<h2>About Me</h2>} />
+        {/* route for an individual user when user queries a specific username */}
+        <Route path={"/users/:username"} element={<UserPage />} />
       </Routes>
     </section>
   );
