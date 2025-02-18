409c3aead76ccce99ddb55375eb69af7f5832a6f
wingchichan <w.chi.chan@gmail.com>
2025-02-18 10:29:36 +0000

    nesting routes and outlet workshop

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -1,9 +1,20 @@
 // react router comes with a hook called useParams that allows us to use params to return specific page/ data
 import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 export default function UserPage() {
   //  storing the useParams in a variable
   let { username } = useParams();
   // whatever username the user enters at the end of the url e.g. /users/homer or /users/spongebob the page will display that param
-  return <h2>User ID: {username}</h2>;
+  return (
+    <>
+      <h2>User ID: {username}</h2>
+
+      <Link to={`/users/${username}`}>Users Profile</Link>
+      <Link to={`/users/${username}/posts`}>Users Posts</Link>
+      <Link to={`/users/${username}/likes`}>Users Likes</Link>
+      <Outlet />
+    </>
+  );
 }
